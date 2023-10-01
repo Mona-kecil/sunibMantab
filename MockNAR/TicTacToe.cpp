94239465c5c543e029ede55b5abf338e5a912dee
Mona-kecil <flowyourminds@gmail.com>
2023-10-02 00:25:00 +0700

    Simple IO, Selection, Looping, 2D Array

--- MockNAR/TicTacToe.cpp
+++ MockNAR/TicTacToe.cpp
@@ -1,128 +1,134 @@
 #include <stdio.h>
 
-int board[4][4], size = 4;
-// Function to initialize the board
+char board[4][4];
+int size = 4;
+int playerIndex = 1;
+
+void cls();
 void init();
 void display();
-void change(int X, int Y, int player);
-void cls();
+void playerMove();
 int checkWin();
 
 int main()
-{	
-	// Initialize the board
-	init();
-	
-	// Visualize the board
-	display();
-		
-	// Loop player until board full or player wins the game	
-	int playerIndex = 1;
-	int isPlaying = 1;
-	
-	while (isPlaying == 1)
-	{
-		printf("\nPlayer %d, enter X and Y coordinate to place your symbol!\n: ", playerIndex);
-		
-		int x, y;
-		do
-		{
-			scanf("%d %d", &y, &x);
-			if (board[x][y] != 0)
-			{
-				printf("\nHey, that place is already taken. Choose another place!\n");
-			}
-		}
-		while(board[x][y] != 0);
-		
-		change(x, y, playerIndex);
-		cls();
-		display();
-		if (playerIndex == 1)
-		{
-			playerIndex++;
-		}
-		else
-		{
-			playerIndex--;
-		}
-		
-		if (checkWin() == 0)
-		
-		{
-			break;
-		}
-	}
-	  
+{
+    init();
+    display();
+    do
+    {
+        playerMove();
+    }
+    while (checkWin() == 0);
+    printf("Player %d wins!", checkWin());
+    return 0;
 }
-
 
 void cls()
 {
-	// Clear screen and display the new board
-	printf("\e[1;1H\e[2J");
+    printf("\e[1;1H\e[2J");
 }
 
 void init()
-{	
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j < size; j++)
-		{
-			if (i == 0 && j == 0)
-			{
-				board[i][j] = NULL;
-			}
-			else if (i == 0 && j > 0)
-			{
-				board[i][j] = j;
-			}
-			else if (i > 0 && j == 0)
-			{
-				board[i][j] = j+i;
-			}
-			else if (i > 0)
-			{
-				board[i][j] = NULL;
-			}
-		}
-	}
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (i == 0 && j == 0)
+            {
+                board[i][j] = 'X';
+            }
+            else if (i == 0 && j > 0)
+            {
+                board[i][j] = j + '0';
+            }
+            else if (i > 0 && j == 0)
+            {
+                board[i][j] = i+j+'0';
+            }
+            else if (i > 0)
+            {
+                board[i][j] = '_';
+            }
+        }
+    }
 }
 
 void display()
 {
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j < size; j++)
-		{
-			printf("%d ", board[i][j]);
-		}
-		printf("\n");
-	}
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-void change(int X, int Y, int player)
+void playerMove()
 {
-	if (player == 1)
-	{
-		board[X][Y] = 5;
-	}
-	else
-	{
-		board[X][Y] = 8;
-	}
+    int x, y;
+    printf("Enter x: ");
+    scanf("%d", &x);
+    printf("Enter y: ");
+    scanf("%d", &y);
+    if (board[x][y] != '_')
+    {
+        printf("Invalid move!\n");
+        return;
+    }
+    if (playerIndex == 1)
+    {
+        board[x][y] = 'X';
+        playerIndex--;
+    }
+    else
+    {
+        board[x][y] = 'O';
+        playerIndex++;
+    }
+    cls();
+    display();
 }
 
-int checkWin() // Fix
+int checkWin()
 {
-	// Rows
-	for (int i = 1; i < size; i++)
-	{
-		if (board[i][1] == 5 && board[i][2] == 5 && board[i][3] == 5)
-		{
-			printf("Player 1 wins!\n");
-			return 0;
-		}
-	}
+    int win = 0;
+    for (int i = 1; i <= size; i++)
+    {
+        if (board[i][1] == 'X' && board[i][2] == 'X' && board[i][3] == 'X')
+        {
+            win = 1;
+        }
+        else if (board[i][1] == 'O' && board[i][2] == 'O' && board[i][3] == 'O')
+        {
+            win = 2;
+        }
+        else if (board[1][i] == 'X' && board[2][i] == 'X' && board[3][i] == 'X')
+        {
+            win = 1;
+        }
+        else if (board[1][i] == 'O' && board[2][i] == 'O' && board[3][i] == 'O')
+        {
+            win = 2;
+        }
+    }
+    if (board[1][1] == 'X' && board[2][2] == 'X' && board[3][3] == 'X')
+    {
+        win = 1;
+    }
+    else if (board[1][1] == 'O' && board[2][2] == 'O' && board[3][3] == 'O')
+    {
+        win = 2;
+    }
+    else if (board[1][3] == 'X' && board[2][2] == 'X' && board[3][1] == 'X')
+    {
+        win = 1;
+    }
+    else if (board[1][3] == 'O' && board[2][2] == 'O' && board[3][1] == 'O')
+    {
+        win = 2;
+    }
+    return win;
 }
-
